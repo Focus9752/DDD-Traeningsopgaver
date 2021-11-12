76674a81327956a5a7a5c3e4352be99afd37be58
Focus9752 <marcuskrogsgaard@outlook.de>
2021-11-12 21:44:36 +0100

    Update main.py

--- November træningsopgaver/metrostationer/main.py
+++ November træningsopgaver/metrostationer/main.py
@@ -6,29 +6,30 @@ def main():
     q = int(lst1[1])
 
     # Lav en list med stationerne og sørg for at objekterne i listen er heltal
-    stationer = input().split(" ")
-    stationer = list(filter(None, stationer))
-    stationer = list(map(int, stationer))
+    global stations
+    stations = input().split(" ")
+    stations = list(filter(None, stations))
+    stations = list(map(int, stations))
         
     # Til debug
     print()
     print("n: " + str(n))
     print("q: " + str(q))
-    print("stationer: ")
-    print(stationer)
+    print("stations: ")
+    print(stations)
     print()
 
-    # Formatér forespørgslerne
-    forespørgsler = []
+    # Formatér forspørgslerne
+    requests = []
 
     i = 0
-    while i < n:
-        forespørgsler.append(list(input().split(" ")))
+    while i < q:
+        requests.append(list(input().split(" ")))
         i += 1
     
     i = 0
-    while i < n:
-        forespørgsler[i] = list(map(int, list(filter(None, forespørgsler[i]))))
+    while i < q:
+        requests[i] = list(map(int, list(filter(None, requests[i]))))
         i += 1
 
     
@@ -36,12 +37,24 @@ def main():
     print()
     print("n: " + str(n))
     print("q: " + str(q))
-    print("stationer: ")
-    print(stationer)
+    print("stations: ")
+    print(stations)
     print()
 
-    print("Forespørgsler: ")
-    print(forespørgsler)
+    print("requests: ")
+    print(requests)
     print()
+    isForwardQuickest(1)
+
+def processRequest(stationPair):
+    1
+
+def isForwardQuickest(stationPair):
+    halfwaypoint = round(n / 2)
+    print(halfwaypoint)
+    
+
 
 main()
+
+
