df9f4fe935244b10a5c883034407982ec280e06f
Focus9752 <marcuskrogsgaard@outlook.de>
2021-11-19 22:34:13 +0100

    Update main.py

--- November træningsopgaver/metrostationer/main.py
+++ November træningsopgaver/metrostationer/main.py
@@ -5,9 +5,9 @@ n = int(lst1[0])
 q = int(lst1[1])
 
 # Store the stations in a list and format them
-stations = input().split(" ")
-stations = list(filter(None, stations))
-stations = list(map(int, stations))
+# stations = input().split(" ")
+# stations = list(filter(None, stations))
+# stations = list(map(int, stations))
     
 # Debug
 # print()
@@ -20,42 +20,56 @@ stations = list(map(int, stations))
 # Format requests
 requests = []
 
-# from ticktock import tick
-# stations = []
-# import random
+from ticktock import tick
+stations = []
+import random
+i = 0
+while i < n:
+    stations.append(random.randint(1,10**9))
+    i += 1
+
+i = 0
+while i < q:
+    requests.append([random.randint(1, n), random.randint(1, n)])
+    i += 1
+
 # i = 0
-# while i < n:
-#     stations.append(random.randint(1,10**9))
+# while i < q:
+#     requests.append(list(input().split(" ")))
 #     i += 1
 
 # i = 0
 # while i < q:
-#     requests.append([random.randint(1, n), random.randint(1, n)])
+#     requests[i] = list(map(int, list(filter(None, requests[i]))))
 #     i += 1
-
-i = 0
-while i < q:
-    requests.append(list(input().split(" ")))
-    i += 1
-
-i = 0
-while i < q:
-    requests[i] = list(map(int, list(filter(None, requests[i]))))
-    i += 1
 
 totalPairs = len(requests)
 totalHeuristics = 0
 
 def main():
-    # clock = tick()
+    heuristicDistances = []
+    slowDistances = []
+
+    clock = tick()
     for pair in requests:
-        # print()
-        # print("current pair: ")
-        # print(pair)
-        # print()
-        getDistance(pair[0], pair[1])
-    # print("Used heuristic in {} out of {} cases ({} %)".format(str(totalHeuristics), str(totalPairs), str(((totalHeuristics / totalPairs) * 100))))
-    # clock.tock()
+        heuristicDistances.append(int(getDistance(pair[0], pair[1])))
+
+    print("Please wait, checking for errors...")
+
+    for pair in requests:
+        slowDistances.append(int(getDistanceSlow(pair[0], pair[1])))
+    clock.tock()
+
+    print("Used heuristic in {} out of {} cases ({} %)".format(str(totalHeuristics), str(totalPairs), str(((totalHeuristics / totalPairs) * 100))))
+
+    errorsFound = getErrors(heuristicDistances, slowDistances)
+
+    print("The heuristic method made {} erros out of {} attempts.".format(errorsFound, len(stations)))
+    print("The error percentage was {}".format(str((errorsFound / len(stations) * 100))) + "%" + " compared to the slower method.")
+
+    
+
+    
 
 # Debug
 # print()
@@ -86,27 +100,13 @@ def getDistance(a, b):
     heuristicVal = heuristic(a, b)
 
     # Heuristic acceptance value
-    acceptanceVal = 0
-    if(len(stations) <= 1000):
-        acceptanceVal = 0.005
-    elif(len(stations) <= 10000):
-        acceptanceVal = 0.10
-    else:
-        acceptanceVal = 0.20
-    
-
-    # The acceptance value is calculated in the heuristic()-method.
-    # It shows the portion of the total stations that a and b cover.
-    #
-    # If the acceptance value is set to some percentage, like 5%, then the heuristic will be used
-    # in all cases where the portion of the stations that lie between a and b are less than 5% of all the stations.
-    #
-    # The proportion of times the heuristic is used is usually around twice the acceptance value, since it applies
-    # to cases where a and b are really close together as well as cases where they are "close together" but on opposite 
-    # ends of the list of stations.
-    #
-    # In our 5% example, the heuristic would apply to both the bottom 5% and top 5% (95%) of cases for the distance between a and b.
-
+    acceptanceVal = 0.25
+    # if(len(stations) <= 1000):
+    #     acceptanceVal = 0.005
+    # elif(len(stations) <= 10000):
+    #     acceptanceVal = 0.10
+    # else:
+    #     acceptanceVal = 0.20
 
     i = a - 1
     j = a - 1
@@ -227,7 +227,88 @@ def getDistance(a, b):
     
     
     print(smallestDistance)
+    return smallestDistance
 
+# Gets distance without using heuristics
+def getDistanceSlow(a, b):
+    forwardLoopFinished = False
+    backwardLoopFinished = False
 
+    forwardDistance = 0
+    backwardDistance = 0
+
+    smallestDistance = 0
+
+    i = a - 1
+    j = a - 1
+
+    count = 0
+
+    while not(forwardLoopFinished and backwardLoopFinished):
+    # Loop for the forward distance
+
+        # If this loop finishes for the first time
+        if((i == b - 1) and not(forwardLoopFinished)):
+            forwardLoopFinished = True
+
+            # If the other loop hasn't finished, then the smallest value is the current value
+            if(not(backwardLoopFinished)):
+                smallestDistance = forwardDistance
+
+            # If it has, then check to see which is smaller and exit the loop
+            elif(forwardDistance <= smallestDistance):
+                smallestDistance = forwardDistance
+                break
+    
+        # Stop the loop if the other loop is finished and has found a smaller value
+        if(backwardLoopFinished):
+            if(forwardDistance >= smallestDistance):
+                forwardLoopFinished = True
+
+        if(i + 1 > n - 1):
+            forwardDistance += stations[n - 1]
+            i = 0
+
+        else:
+            forwardDistance += stations[i]
+            i += 1
+
+        # Loop for the backward distance
+
+        if((j == b - 1) and not(backwardLoopFinished)):
+            backwardLoopFinished = True
+
+            if(not(forwardLoopFinished)):
+                smallestDistance = backwardDistance
+
+            elif(backwardDistance <= smallestDistance):
+                smallestDistance = backwardDistance
+                break
+            
+        # Stop the loop if the other loop is finished and has found a smaller value
+        if(forwardLoopFinished):
+            if(backwardDistance >= smallestDistance):
+                backwardLoopFinished = True
+
+        if(j - 1 == (-1)):
+            j = n - 1
+            backwardDistance += stations[n - 1]
+        
+        else:
+            j -= 1
+            backwardDistance += stations[j]
+
+        count += 1
+    
+    return smallestDistance
+    
+def getErrors(fastDist, slowDist):
+    errors = 0
+    for index in range(len(fastDist)):
+        if(fastDist[index] != slowDist[index]):
+            errors += 1
+    
+    return errors
+    
 
 main()
