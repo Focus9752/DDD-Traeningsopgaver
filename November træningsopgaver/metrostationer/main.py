004661984f01b9a889a4619f49ef42dde11b4414
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2021-11-28 22:05:31 +0100

    .

--- November træningsopgaver/metrostationer/main.py
+++ November træningsopgaver/metrostationer/main.py
@@ -1,27 +1,13 @@
-import random
-from ticktock import tick
-import time
-
-# Seperate n and q
+# Input
 lst1 = input()
 lst1 = lst1.split(" ")
 n = int(lst1[0])
 q = int(lst1[1])
 
-# Store the stations in a list and format them
 stations = input().split(" ")
 stations = list(filter(None, stations))
 stations = list(map(int, stations))
-    
-# Debug
-# print()
-# print("n: " + str(n))
-# print("q: " + str(q))
-# print("stations: ")
-# print(stations)
-# print()
 
-# Format requests
 requests = []
 
 i = 0
@@ -35,23 +21,66 @@ while i < q:
     i += 1
 
 totalPairs = len(requests)
-totalHeuristics = 0
+
 
 def main():
+    sumList = [sum(stations[ : i + 1]) for i in range(len(stations))]
+    totalLength = sumList[len(stations) - 1]
 
-    totalLength = None
-    for i in range(len(stations)):
-        totalLength += stations[i]
-        
+    for pair in requests:
+        print(getDistance(pair[0] - 1, pair[1] - 1, sumList, totalLength))
+         
+def getDistance(a, b, sumList, totalLength):
+    if(a < b):
+        small = a
+        big = b
+    else:
+        small = b
+        big = a
+
+    # Hvis vi ikke skal bevæge os
+    if(small == big):
+        return 0
+
+    # Hvis begge stationer ligger i "enderne" af metroen
+    if small == 0 and big == len(stations) - 1:
+        forwardDistance = sumList[big - 1]
+
+    # Hvis vi skal fremad
+    if small == 0:
+        forwardDistance = sumList[big - 1]
     
-def getDistance(a,b):
-    a -= 1
-    b -= 1
+    # Hvis vi skal bagud
+    if big == len(stations) - 1:
+        forwardDistance = sumList[big - 1] - sumList[small - 1]
 
-    shortestDist = None
-    
-    while shortestDist < totalHeuristics / 2:
+    backwardDistance = totalLength - forwardDistance
 
-# slå op: python sum prefix!!!
+    if(backwardDistance > forwardDistance):
+        return forwardDistance
+    else:
+        return backwardDistance
+
+# 1 2 3 4 5
+# 1 3 6 10 15
+# 1 2
+# 2 5
+# 5 4
+# 2 2
+
+# 1
+# 6
+# 4
+# 0
+
+# 3 7 1 20
+# 3 10 11 31
+# 1 2
+# 1 3
+# 2 4
+
+# 3
+# 10
+# 8
 
 main()
