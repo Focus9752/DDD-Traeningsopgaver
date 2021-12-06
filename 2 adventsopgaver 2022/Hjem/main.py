2f2d1cebe77a111e167fea40aa15cc3fdcd31171
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2021-12-06 13:57:42 +0100

    .

---
+++ 2 adventsopgaver 2022/Hjem/main.py
@@ -0,0 +1,9 @@
+def main():
+    n = int(input())
+    bus1 = list(map(int(input().split())))
+    trains = list(map(int(input().split())))
+    bus2 = list(map(int(input().split())))
+
+    print(n,bus1,trains,bus2)
+
+main()
