cd9dff376f5e1ca3c8d8285a60000f226853a6dd
Focus9752 <marcuskrogsgaard@outlook.de>
2021-12-06 21:06:34 +0100

    Update main.py

--- 2 adventsopgaver 2022/Hjem/main.py
+++ 2 adventsopgaver 2022/Hjem/main.py
@@ -1,9 +1,22 @@
 def main():
     n = int(input())
-    bus1 = list(map(int(input().split())))
-    trains = list(map(int(input().split())))
-    bus2 = list(map(int(input().split())))
+    bus1 = list(map(int,(input().split())))
+    trains = list(map(int,(input().split())))
+    bus2 = list(map(int,(input().split())))
 
-    print(n,bus1,trains,bus2)
+    trains.append(0)
 
+    distances = []
+
+    for i in range(n):
+        distances.append(bus1[i] + trains[i] + bus2[i])
+
+    shortestDistance = distances[0]
+
+    for i in range(len(distances)):
+        if distances[i] <= shortestDistance:
+            shortestDistance = distances[i]
+
+    print(shortestDistance)
+        
 main()
