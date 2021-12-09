a31cecf7effed4fb6a32c5a9ae824d63bc6fb154
Focus9752 <marcuskrogsgaard@outlook.de>
2021-12-09 18:12:55 +0100

    endelig implementeret dijkstra

--- 2 adventsopgaver 2022/Hjem/main.py
+++ 2 adventsopgaver 2022/Hjem/main.py
@@ -1,13 +1,32 @@
-# we store the edges in a matrix where the rows and collumns are nodes and the values are 
-# the weights of the edges. For example:
-#    0 1 2
-#    ------
-# 0 | 1 2 3
-# 1 | 1 1 1
-# 2 | 8 6 4
+from collections import namedtuple
 
-import graphs
-from queue import PriorityQueue
+Graph = namedtuple("Graph", ["nodes", "edges","is_directed"])
+
+def adjacency_dict(graph):
+    adj = {node: {} for node in graph.nodes}
+    for edge in graph.edges:
+        node1, node2, weigth = edge[0], edge[1], edge[2]
+        adj[node1][node2] = weigth
+        if not graph.is_directed:
+            adj[node2][node1] = weigth
+    
+    return adj
+
+def adjacency_matrix(graph):
+    adj = [[-1 for node in graph.nodes] for node in graph.nodes]
+    for edge in graph.edges:
+        node1, node2, weigth = edge[0], edge[1], edge[2]
+        adj[node1][node2] = weigth
+        if not graph.is_directed:
+            adj[node2][node1] = weigth
+    
+    return adj
+
+def matrixPrint(mat):
+    print(" ", " ".join([str(x) for x in range(len(mat))]))
+    for i, x in enumerate(mat):
+        print (i, " ".join([str(y) for y in x]))  # or  if elements are not string
+
 
 
 n = int(input())
@@ -27,61 +46,71 @@ for i in range(len(nodes)):
     # bus1list
     if i < n:
         edges.append((i, n + i, bus1list[i]))
+        edges.append((3 * n, i, 0))
 
     # trains
     elif i < 2 * n:
         # fires if we are at the last train station in the list
-        # we can't go to the next station
+        # we can't go to the next station so we only append the bus to the destination
         if i == 2 * n - 1:
-            pass
+            edges.append((i, n + i, bus2list[i - n]))
 
         else:
             edges.append((i, i + 1, trains[i - n]))
+            edges.append((i, n + i, bus2list[i - n]))
 
     # bus2list
     elif i < 3 * n:
-        edges.append((i, i - n, bus2list[i - 2 * n]))
-    
-    # add the start and end nodes
-    else:
-        for j in range(n):
-            edges.append((n * 3, j, bus1list[j]))
-            edges.append((n * 3 + 1, j + 2 * n, bus2list[j]))
+        edges.append((i, 3 * n + 1, 0))
 
 # converts the graph to an adjacency matrix
-g_mat = graphs.adjacency_matrix(graphs.Graph(nodes,edges,True))
+g_mat = adjacency_matrix(Graph(nodes,edges,False))
 
 # converts the graph to an adjacency dictionary
-g_dict = graphs.adjacency_dict(graphs.Graph(nodes,edges,True))
+g_dict = adjacency_dict(Graph(nodes,edges,False))
 
 #graphs.matrixPrint(g_mat)
-print(g_dict)
 
 distances = g_dict
 
-unvisited = {node: None for node in nodes} #using None as +inf
-visited = {}
-current = 0
-currentDistance = 0
-unvisited[current] = currentDistance
+def dijkstra(nodes, edges, source, destination, is_directed=False):
+    # initialize pathlength as a dict with a path length for each node
+    # at the start all path lengths are inf
+    pathlengths = {n: float("inf") for n in nodes}
+    # we set the path length of the source node to 0
+    pathlengths[source] = 0
 
-while True:
-    for neighbour, distance in distances[current].items():
-        if neighbour not in unvisited: continue
-        newDistance = currentDistance + distance
-        if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
-            unvisited[neighbour] = newDistance
-    visited[current] = currentDistance
-    del unvisited[current]
-    if not unvisited: break
-    candidates = [node for node in unvisited.items() if node[1]]
-    current, currentDistance = sorted(candidates, key = lambda x: x[1])[0]
+    # # we initialize a nested dictionary to store the adjacent nodes for each node
+    # adjacent_nodes = {n: {} for n in nodes}
+    # # we add adjacent nodes to the dictionary
+    # for (u, v), w_uw in edges.items():
+    #     adjacent_nodes[u][v] = w_uw
+    #     adjacent_nodes[v][u] = w_uw
 
-print(visited)
+    adjacent_nodes = adjacency_dict(Graph(nodes,edges,is_directed))
 
+    # this list will contain nodes that we have yet to check
+    # the program is done once the list is empty
+    temporary_nodes = [n for n in nodes]
+    while len(temporary_nodes) > 0:
+        # we find the path length to each node in the list of remaining nodes
+        upper_bounds = {n: pathlengths[n] for n in temporary_nodes}
+        # and find the node with the smallest path length
+        u = min(upper_bounds, key=upper_bounds.get)
 
-# for vertex in range(len(D)):
-#     print("Distance from vertex 0 to vertex", vertex, "is", D[vertex])
+        # we remove the smallest node (the node that we are considering)
+        # from the list of temporary nodes that we have yet to check
+        temporary_nodes.remove(u)
 
+        # we look at all the adjacent nodes for u and update their path lengths
+        for n, weigth in adjacent_nodes[u].items():
+            pathlengths[n] = min(pathlengths[n], pathlengths[u] + weigth)
+        
+        if u == destination:
+            return pathlengths[u]
+    
+    return pathlengths
+    
+dist_to_goal = dijkstra(nodes, edges, nodes[n * 3], nodes[n * 3 + 1], True)
 
-
+print(dist_to_goal)
