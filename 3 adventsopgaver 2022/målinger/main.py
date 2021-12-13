d0fb19840e45bf21f003071f9622f138ae7451bf
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2021-12-13 17:06:04 +0100

    .

--- 3 adventsopgaver 2022/målinger/main.py
+++ 3 adventsopgaver 2022/målinger/main.py
@@ -2,59 +2,56 @@ n,k = list(map(int, input().split()))
 
 # raw input as a list of tuples
 # [(5, 50), (6, 55) (7, 60)]
-inputNumbers = []
+inputyValues = []
 
 for i in range(n):
     temp_list = list(map(int, input().split()))
-    inputNumbers.append((temp_list[0], temp_list[1]))
+    inputyValues.append((temp_list[0], temp_list[1]))
 
-# the numbers from the input
+# a sorted list containing the y values from the input
 # [50, 55, 60]
-numbers = [inputNumbers[i][1] for i in range(len(inputNumbers))]
+yValues = [inputyValues[i][1] for i in range(len(inputyValues))]
+yValues.sort()
 
-inputNumbersDict = {inputNumbers[i][1]: inputNumbers[i][0] for i in range(len(inputNumbers))}
+# a dicitonary of corresponding y and x values {y: x}
+inputyValuesDict = {inputyValues[i][1]: inputyValues[i][0] for i in range(len(inputyValues))}
+inputyValuesDict = {yValues[i]: inputyValuesDict[yValues[i]] for i in range(len(yValues))}
+
 # store requests
 requests = []
-
 for i in range(k):
     requests.append(int(input()))
 
 def main():
     for q in requests:
-        # add q to the list of y values and sort it
-        temp_list = list(numbers)
-        temp_list.append(q)
-        temp_list.sort()
-
-        newindex = temp_list.index(q)
-        # if q is placed last, then there are no y bigger than or equal to it
-        if newindex == len(temp_list) - 1:
+        # use binary search to find the index we should place q at in order to keep the list sorted
+        newindex = find_index(yValues, q)
+        # if that index is greater than the max index, then there are no y-values larger than q
+        # which means we print -1
+        if newindex > len(yValues) - 1:
             print(-1)
         else:
-            for i in range(newindex,len(temp_list)):
-                targetnum = temp_list[i]
-                if temp_list[i] >= q and targetnum in numbers:
-                    break
-            print(inputNumbersDict[targetnum])
+            # we look up the corresponding x value in our dicitonary
+            print(inputyValuesDict[yValues[newindex]])
 
-def binarySearch(A, elem, l, r):
-    if l <= r:
-        m = (l + r + 1) // 2
+# binary search implementation to find the index 
+# where a new number should be placed in a sorted list
+def find_index(list, K):
+    l = 0
+    r = len(list) - 1
 
-        if l == r:
-            return l
+    while l <= r:
+        mid = (l + r) // 2
 
-        elif A[m] > elem:
-            return binarySearch(A, elem, l, m - 1)
+        if list[mid] == K:
+            return mid
+
+        elif list[mid] < K:
+            l = mid + 1
         
         else:
-            return binarySearch(A, elem, m, r)
-
-def S(x, q):
-    if x >= q:
-        return True
-    else:
-        return False
+            r = mid - 1
+    
+    return r + 1
 
 main()
-
