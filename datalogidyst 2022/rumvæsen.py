d1e8f46d4d22de504926c8d6a686e176f97db25c
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2022-01-13 21:10:06 +0100

    Update rumvæsen.py

--- datalogidyst 2022/rumvæsen.py
+++ datalogidyst 2022/rumvæsen.py
@@ -1,11 +1,61 @@
+from audioop import reverse
 from collections import namedtuple
 import itertools
 
-cycle = namedtuple("cycle", ["startIndex", "endIndex", "length"])
-
 def main():
-    pass
+    cyclesList = findCycles()
 
+    for i in range(len(cyclesList)):
+        for j in range(len(cyclesList)):
+            if len(cyclesList[i]) % len(cyclesList[j]) == 0:
+                 cyclesList.remove(cyclesList[j])
+    
+    for i in range(len(qList)):
+        name[qList[i]] = cList[i]
+        result = 0
+
+        cycleLenghts = []
+        for cycle in cyclesList:
+            cycleLenghts.append(getUniqueStrings(cycle))
+
+        result = 1
+        for i in range(len(cycleLenghts)):
+            result *= cycleLenghts[i]
+    
+        print(result)
+
+def getUniqueStrings(cycle):
+    cycleKeys = list(cycle.keys())
+    firstLetter = cycleKeys[0]
+
+    for index in cycleKeys:
+        if name[index] != firstLetter:
+            return len(cycle)
+    
+    return 1
+
+def findCycles():
+    cyclesListTemp = []
+    currentCycle = dict()
+    currentIndex = -1
+
+    while len(name_P_dict) > 0:
+        currentCycle = dict()
+        currentIndex = list(name_P_dict.keys())[0]
+        startIndex = currentIndex
+
+        while True:
+            currentCycle[currentIndex + 1] = P[currentIndex] + 1
+            del(name_P_dict[currentIndex])
+            currentIndex = P[currentIndex]
+
+            if currentIndex == startIndex:
+                break
+
+        cyclesListTemp.append(currentCycle)
+
+    return cyclesListTemp
+        
 def calculateStarScore(S, P, score, original_S):
     S = list(S)
     T = S.copy()
@@ -30,15 +80,24 @@ def changeName(S, q, c):
     return S
 
 N, Q = list(map(int, input().split()))
-name = input()
+name = list(input())
 P = list(map(int, input().split()))
+P = [num - 1 for num in P]
+
+name_P_dict = dict()
+
+for i in range(len(name)):
+    name_P_dict[i] = P[i]
+
+{0: 2,
+1: 3}
 
 qList = []
 cList = []
 
 for i in range(Q):
     inputList = input().split()
-    qList.append(int(inputList[0]))
+    qList.append(int(inputList[0]) - 1)
     cList.append(str(inputList[1]))
 
 main()
